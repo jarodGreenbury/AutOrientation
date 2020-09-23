c108f06e2a8d7412f39d55b0b990223bd1f1858c
jarodGreenbury <jarodGreenbury@users.noreply.github.com>
2020-09-23 23:05:12 +1200

    add

---
+++ AUTOrientation/Assets/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameEnded = false;
+
+    public void EndGame ()
+    {
+        if (gameEnded == false)
+        {
+            gameEnded = true;
+            Restart();
+        }
+    }
+
+    private void Restart ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
+
