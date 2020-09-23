39fa6f0c91e43220372dd6bf08f1932b91870f8d
garry332211 <68804397+garry332211@users.noreply.github.com>
2020-09-24 09:44:04 +1200

    Player added

---
+++ Third Person Player/Assets/Scripts/GarryControls.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GarryControls : MonoBehaviour
+{
+    static Animator anim2;
+    public float speed = 10.0f;
+    public float roationSpeed = 100.0f;
+    void Start()
+    {
+        anim2 = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * roationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+        if (Input.GetButtonDown("Jump"))
+        {
+            anim2.SetTrigger("isJumping");
+        }
+
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            anim2.SetTrigger("isJumping");
+        }
+        if (translation != 0)
+        {
+            anim2.SetBool("isRunning", true);
+            anim2.SetBool("isIdle", false);
+        }
+
+        else
+        {
+            anim2.SetBool("isRunning", false);
+            anim2.SetBool("isIdle", true);
+        }
+
+
+
+    }
+
+    }
+
