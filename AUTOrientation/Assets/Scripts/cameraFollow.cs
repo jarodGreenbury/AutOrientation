bafdeff0e0683da5026c36f919b34c5873f43411
jarodGreenbury <jarodGreenbury@users.noreply.github.com>
2020-09-23 14:40:55 +1200

    add

---
+++ AUTOrientation/Assets/Scripts/cameraFollow.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+    public Vector3 rotOffset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.eulerAngles = player.rotation.eulerAngles.y * rotOffset;
+        transform.position = player.position + offset;
+
+
+        
+    }
+}
