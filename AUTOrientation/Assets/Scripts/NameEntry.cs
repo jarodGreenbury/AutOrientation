3afd7c7bd894df1db0e8a5031af80abd9bbf8313
matthew-blenkinsopp <70620724+matthew-blenkinsopp@users.noreply.github.com>
2020-09-24 23:51:52 +1200

    Added playername
    Player can now enter their name in the menu before starting the game. Name currently only displayed in debug window upon starting the game. name stored in static string 'username' in menu scene

---
+++ AUTOrientation/Assets/Scripts/NameEntry.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+public class NameEntry : MonoBehaviour
+{
+    public string playerName;
+    public GameObject inputField;
+    public static string username;
+
+    public void StoreName()
+    {
+        playerName = inputField.GetComponent<Text>().text; 
+        username = playerName;
+        UnityEngine.Debug.Log("Name: " + username);
+    }
+}
