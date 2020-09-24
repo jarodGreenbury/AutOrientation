0a7cfca3721e85e1a41a0b960245c8681f86a249
jarodGreenbury <jarodGreenbury@users.noreply.github.com>
2020-09-24 14:58:06 +1200

    Game Manager
    Connect the scenes

---
+++ AUTOrientation/Assets/Scripts/Running.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Running : MonoBehaviour
+{
+    static Animator anim2;
+    public float speed = 10.0f;
+    public float runningSpeed = 20.0f;
+    public float roationSpeed = 100.0f;
+    void Start()
+    {
+        anim2 = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        bool running = Input.GetKey(KeyCode.LeftShift);
+
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * roationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+        if (Input.GetButtonDown("Jump"))
+        {
+            anim2.SetTrigger("isJumping");
+        }
+
+        else if (translation != 0)
+        {
+            if (running)
+            {
+                speed = 20.0f;
+                anim2.SetBool("isSprinting", true);
+                anim2.SetBool("isRunning", false);
+                anim2.SetBool("isIdle", false);
+
+            }
+            else
+            {
+                speed = 10.0f;
+                anim2.SetBool("isSprinting", false);
+                anim2.SetBool("isRunning", true);
+                anim2.SetBool("isIdle", false);
+            }
+        }
+
+
+        else
+        {
+            anim2.SetBool("isRunning", false);
+            anim2.SetBool("isIdle", true);
+        }
+
+
+
+    }
+
+}
+
---
+++ AUTOrientation/Assets/Scripts/trigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class trigger : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+    
+         Debug.Log("we hit");
+         FindObjectOfType<GameManager>().LoadCarPark();
+    }
+}
