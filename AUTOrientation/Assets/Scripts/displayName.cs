9f62b583462f658c9e90500f5d5fe89fe30a471c
matthew-blenkinsopp <70620724+matthew-blenkinsopp@users.noreply.github.com>
2020-09-26 10:50:20 +1200

    fixed name entry

---
+++ AUTOrientation/Assets/Scripts/displayName.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class displayName : MonoBehaviour
+{
+    public Text nameDisplay;
+    
+
+    // Update is called once per frame, therefore name is being updated every frame. Must be a better way to do this.
+    void Update()
+    {
+        nameDisplay.GetComponent<Text>().text = "Name: " + enterName.username;
+    }
+}
