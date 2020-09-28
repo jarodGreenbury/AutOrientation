f27e7816f644e70781af3b28df91fc1f1424a181
jarodGreenbury <jarodGreenbury@users.noreply.github.com>
2020-09-28 23:22:34 +1300

    Day-Night Cycle

---
+++ AUTOrientation/Assets/sun.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sun : MonoBehaviour
+{
+    //is the speed of the day
+    public float speed;
+    public float nightSpeed;
+    public Transform sunLight;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sunLight.position.y < -350)
+        {
+            Debug.Log("speed 20");
+            speed = 20f;
+        }
+        else
+        {
+            speed = 5f;
+        }
+        //code changes the position and rotation of the sun(directional light)
+        transform.RotateAround(Vector3.zero, Vector3.right, speed * Time.deltaTime);
+        transform.LookAt(Vector3.zero);
+    }
+}
