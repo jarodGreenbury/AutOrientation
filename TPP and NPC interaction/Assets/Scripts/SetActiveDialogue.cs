9a549d64b7d91244713a4c63e18b560c03735a1e
garry332211 <68804397+garry332211@users.noreply.github.com>
2020-09-30 13:31:02 +1300

    Added TPP & NPC with interaction

---
+++ TPP and NPC interaction/Assets/Scripts/SetActiveDialogue.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetActiveDialogue : MonoBehaviour
+{
+    public GameObject NPCDialogue;
+    public GameObject PlayerDialogue;
+    private bool isCollided;
+    private GarryMovement movement;
+
+  
+    private void Start()
+    {
+      
+        movement = GetComponent<GarryMovement>();
+        NPCDialogue.SetActive(false);
+        PlayerDialogue.SetActive(false);
+    }
+    public void Update()
+    {
+        
+        Debug.Log(isCollided);
+        if(Input.GetKeyDown(KeyCode.Q) && isCollided==true)
+        {
+            isCollided = false;
+            movement.speed = 2;
+            NPCDialogue.SetActive(false);
+            PlayerDialogue.SetActive(false);
+        }
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("SetActiveDialogue"))
+        {
+            movement.speed = 0;
+            isCollided = true;
+            NPCDialogue.SetActive(true);
+            PlayerDialogue.SetActive(true);
+
+        }   
+        
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        isCollided = false;
+        NPCDialogue.SetActive(false);
+        PlayerDialogue.SetActive(false);
+    }
+
+
+
+}
