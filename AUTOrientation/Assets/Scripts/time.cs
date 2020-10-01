bfd319d92a5c07016fc74ed05edc91f812ed267e
jarodGreenbury <jarodGreenbury@users.noreply.github.com>
2020-10-01 19:49:11 +1300

    Lamps added

--- AUTOrientation/Assets/Scripts/time.cs
+++ AUTOrientation/Assets/Scripts/time.cs
@@ -3,6 +3,12 @@ using System.Collections;
 using UnityEngine.UI;
 using System;
 
+/* Author: Jarod Greenbury
+ * Date Modified : 1/10/20
+ * Purpose: This script controls the in game clock. The purpose of this script is 
+ * to control the clock which provides ques for other scripts. Eg. lamps.
+ */
+
 public class time : MonoBehaviour
 {
     public Text clock;
@@ -17,10 +23,10 @@ public class time : MonoBehaviour
 
     void Update()
     {
-        //UnityEngine.Debug.Log(today);
+        //calls function to update the clocks time
         ChangeClock();
         timeString = today.ToString("HH:mm");
-        clock.text = ("Day: " + day + " Time: " + timeString).ToString();
+        clock.text = ("Day: " + day + " Time: " + timeString).ToString(); //display the time to the GUI
         
        //yield WaitForSeconds(0.2f);
         
@@ -28,6 +34,7 @@ public class time : MonoBehaviour
 
     void ChangeClock()
     {
+        // counts the days passed in game. Based off 24 hr clock
         if (today.Hour == 0 && dayChanged == false)
         {
             day += 1;
@@ -38,7 +45,7 @@ public class time : MonoBehaviour
             dayChanged = false;
         }
 
-        TimeSpan newTime = new TimeSpan(0, 0, 0, 6);
+        TimeSpan newTime = new TimeSpan(0, 0, 0, 3);
         if (!pauseMenu.gameIsPaused)
         {
             today = today + newTime;
